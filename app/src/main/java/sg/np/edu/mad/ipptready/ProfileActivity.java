17ed8596aa71ebf7ff69007c5f728b4d7791a1d7
kuanzher <s10223870@connect.np.edu.sg>
2022-06-11 20:33:09 +0800

    Retrieved user profile details and display

--- app/src/main/java/sg/np/edu/mad/ipptready/ProfileActivity.java
+++ app/src/main/java/sg/np/edu/mad/ipptready/ProfileActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,11 +28,35 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        Toast GenericErrorToast = Toast.makeText(this,
+                "Unexpected error occurred",
+                Toast.LENGTH_SHORT);
 
         Intent intent = getIntent();
         EmailAddress = intent.getStringExtra("Email");
         SerializedUser = intent.getByteArrayExtra("User");
+
+        User user = null;
+        ByteArrayInputStream bis = new ByteArrayInputStream(SerializedUser);
+        try {
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            // casting will work 100%! Clueless
+            user = (User)ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            // show generic error message ...
+
+            GenericErrorToast.show();
+            e.printStackTrace();
+            finish();
+        }
+
+        TextView name = findViewById(R.id.name);
+        TextView email = findViewById(R.id.email);
+        TextView dob = findViewById(R.id.dateOfBirth);
+
+        name.setText(user.Name);
+        email.setText(EmailAddress);
+        dob.setText(user.DOB.toString());
 
     }
 
