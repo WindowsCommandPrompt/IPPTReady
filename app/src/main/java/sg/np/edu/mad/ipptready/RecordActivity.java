7ba5c202d6e6ccf1c2ab98e9c091e3024ec3ba1a
Joshua Ng <joshuang21978@gmail.com>
2022-06-14 15:44:01 +0800

    Resolved Error in RoutineActivity

--- app/src/main/java/sg/np/edu/mad/ipptready/RecordActivity.java
+++ app/src/main/java/sg/np/edu/mad/ipptready/RecordActivity.java
@@ -79,86 +79,84 @@ public class RecordActivity extends AppCompatActivity {
                     GenericErrorToast.show();
                     e.printStackTrace();
                     finish();
-                }
-                ipptRoutine.getRecordsList(EmailAddress,
-                                        IPPTCycleId,
-                                        new OnCompleteListener<QuerySnapshot>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                if (task.isSuccessful()) {
-                                                    if (!task.getResult().isEmpty()) {
-                                                        for (DocumentSnapshot document : task.getResult()) {
-                                                            if ("RunRecord" == document.getId()) {
-                                                                RunRecord runRecord = document.toObject(RunRecord.class);
-                                                                findViewById(R.id.runrecordButton).setVisibility(View.GONE);
-                                                                ((TextView)findViewById(R.id.runrecordtotaldistancetravelled)).setText(String.valueOf(runRecord.TotalDistanceTravelled) + "km");
-                                                                ((TextView)findViewById(R.id.runrecordtimetakentotal)).setText(SecondstoString(runRecord.TimeTakenTotal));
-                                                                ((TextView)findViewById(R.id.runrecordtimetakenfinished)).setText(SecondstoString(runRecord.TimeTakenFinished));
-                                                            }
-                                                            else if ("SitupRecord" == document.getId()) {
-                                                                SitupRecord situpRecord = document.toObject(SitupRecord.class);
-                                                                findViewById(R.id.situprecordButton).setVisibility(View.GONE);
-                                                                ((TextView)findViewById(R.id.situprecordnumreps)).setText(String.valueOf(situpRecord.NumsReps));
-                                                                ((TextView)findViewById(R.id.situprecordrepstarget)).setText(String.valueOf(situpRecord.RepsTarget));
-                                                            }
-                                                            else if ("PushupRecord" == document.getId()) {
-                                                                PushupRecord pushupRecord = document.toObject(PushupRecord.class);
-                                                                findViewById(R.id.pushuprecordButton).setVisibility(View.GONE);
-                                                                ((TextView)findViewById(R.id.pushuprecordnumreps)).setText(String.valueOf(pushupRecord.NumsReps));
-                                                                ((TextView)findViewById(R.id.pushuprecordrepstarget)).setText(String.valueOf(pushupRecord.RepsTarget));
-                                                            }
-                                                        }
-                                                        /*if (View.GONE != findViewById(R.id.runrecordButton).getVisibility()) {
-                                                            findViewById(R.id.runrecordButton).setOnClickListener(new View.OnClickListener() {
-                                                                @Override
-                                                                public void onClick(View v) {
-                                                                    Intent recordIntent = new Intent(RecordActivity.this, RunActivity.class);
-
-                                                                    recordIntent.putExtra("Email", EmailAddress);
-                                                                    recordIntent.putExtra("IPPTCycleId", IPPTCycleId);
-                                                                    recordIntent.putExtra("IPPTRoutineId", IPPTRoutineId);
-                                                                    recordIntent.putExtra("IPPTRecordId", "RunRecord");
-
-                                                                    startActivity(recordIntent);
-                                                                }
-                                                            });
-                                                        }
-                                                        if (View.GONE != findViewById(R.id.situprecordButton).getVisibility()) {
-                                                            findViewById(R.id.situprecordButton).setOnClickListener(new View.OnClickListener() {
-                                                                @Override
-                                                                public void onClick(View v) {
-                                                                    Intent recordIntent = new Intent(RecordActivity.this, SitupActivity.class);
-
-                                                                    recordIntent.putExtra("Email", EmailAddress);
-                                                                    recordIntent.putExtra("IPPTCycleId", IPPTCycleId);
-                                                                    recordIntent.putExtra("IPPTRoutineId", IPPTRoutineId);
-                                                                    recordIntent.putExtra("IPPTRecordId", "SitupRecord");
-
-                                                                    startActivity(recordIntent);
-                                                                }
-                                                            });
-                                                        }
-                                                        if (View.GONE != findViewById(R.id.pushuprecordButton).getVisibility()) {
-                                                            findViewById(R.id.pushuprecordButton).setOnClickListener(new View.OnClickListener() {
-                                                                @Override
-                                                                public void onClick(View v) {
-                                                                    Intent recordIntent = new Intent(RecordActivity.this, PushupActivity.class);
-
-                                                                    recordIntent.putExtra("Email", EmailAddress);
-                                                                    recordIntent.putExtra("IPPTCycleId", IPPTCycleId);
-                                                                    recordIntent.putExtra("IPPTRoutineId", IPPTRoutineId);
-                                                                    recordIntent.putExtra("IPPTRecordId", "PushupRecord");
-
-                                                                    startActivity(recordIntent);
-                                                                }
-                                                            });
-                                                        }*/
+        }
+        ipptRoutine.getRecordsList(EmailAddress,
+                                IPPTCycleId,
+                                new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            if (!task.getResult().isEmpty()) {
+                                                for (DocumentSnapshot document : task.getResult()) {
+                                                    if ("RunRecord" == document.getId()) {
+                                                        RunRecord runRecord = document.toObject(RunRecord.class);
+                                                        findViewById(R.id.runrecordButton).setVisibility(View.GONE);
+                                                        ((TextView)findViewById(R.id.runrecordtotaldistancetravelled)).setText(String.valueOf(runRecord.TotalDistanceTravelled) + "km");
+                                                        ((TextView)findViewById(R.id.runrecordtimetakentotal)).setText(SecondstoString(runRecord.TimeTakenTotal));
+                                                        ((TextView)findViewById(R.id.runrecordtimetakenfinished)).setText(SecondstoString(runRecord.TimeTakenFinished));
+                                                    }
+                                                    else if ("SitupRecord" == document.getId()) {
+                                                        SitupRecord situpRecord = document.toObject(SitupRecord.class);
+                                                        findViewById(R.id.situprecordButton).setVisibility(View.GONE);
+                                                        ((TextView)findViewById(R.id.situprecordnumreps)).setText(String.valueOf(situpRecord.NumsReps));
+                                                        ((TextView)findViewById(R.id.situprecordrepstarget)).setText(String.valueOf(situpRecord.RepsTarget));
+                                                    }
+                                                    else if ("PushupRecord" == document.getId()) {
+                                                        PushupRecord pushupRecord = document.toObject(PushupRecord.class);
+                                                        findViewById(R.id.pushuprecordButton).setVisibility(View.GONE);
+                                                        ((TextView)findViewById(R.id.pushuprecordnumreps)).setText(String.valueOf(pushupRecord.NumsReps));
+                                                        ((TextView)findViewById(R.id.pushuprecordrepstarget)).setText(String.valueOf(pushupRecord.RepsTarget));
                                                     }
                                                 }
+                                                /*if (View.GONE != findViewById(R.id.runrecordButton).getVisibility()) {
+                                                    findViewById(R.id.runrecordButton).setOnClickListener(new View.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(View v) {
+                                                            Intent recordIntent = new Intent(RecordActivity.this, RunActivity.class);
+
+                                                            recordIntent.putExtra("Email", EmailAddress);
+                                                            recordIntent.putExtra("IPPTCycleId", IPPTCycleId);
+                                                            recordIntent.putExtra("IPPTRoutineId", IPPTRoutineId);
+                                                            recordIntent.putExtra("IPPTRecordId", "RunRecord");
+
+                                                            startActivity(recordIntent);
+                                                        }
+                                                    });
+                                                }
+                                                if (View.GONE != findViewById(R.id.situprecordButton).getVisibility()) {
+                                                    findViewById(R.id.situprecordButton).setOnClickListener(new View.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(View v) {
+                                                            Intent recordIntent = new Intent(RecordActivity.this, SitupActivity.class);
+
+                                                            recordIntent.putExtra("Email", EmailAddress);
+                                                            recordIntent.putExtra("IPPTCycleId", IPPTCycleId);
+                                                            recordIntent.putExtra("IPPTRoutineId", IPPTRoutineId);
+                                                            recordIntent.putExtra("IPPTRecordId", "SitupRecord");
+
+                                                            startActivity(recordIntent);
+                                                        }
+                                                    });
+                                                }
+                                                if (View.GONE != findViewById(R.id.pushuprecordButton).getVisibility()) {
+                                                    findViewById(R.id.pushuprecordButton).setOnClickListener(new View.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(View v) {
+                                                            Intent recordIntent = new Intent(RecordActivity.this, PushupActivity.class);
+
+                                                            recordIntent.putExtra("Email", EmailAddress);
+                                                            recordIntent.putExtra("IPPTCycleId", IPPTCycleId);
+                                                            recordIntent.putExtra("IPPTRoutineId", IPPTRoutineId);
+                                                            recordIntent.putExtra("IPPTRecordId", "PushupRecord");
+
+                                                            startActivity(recordIntent);
+                                                        }
+                                                    });
+                                                }*/
                                             }
-                                        });
-            }
-        }
+                                        }
+                                    }
+                                });
     }
 
     private String SecondstoString(int seconds) {
