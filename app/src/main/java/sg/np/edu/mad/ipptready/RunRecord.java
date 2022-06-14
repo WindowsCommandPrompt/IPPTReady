a9febd651216b40ba8afbd10962158d7c7d8a49d
WindowsCommandPrompt <masterzy1234@gmail.com>
2022-06-14 20:00:25 +0800

    modified RunRecord.java

--- app/src/main/java/sg/np/edu/mad/ipptready/RunRecord.java
+++ app/src/main/java/sg/np/edu/mad/ipptready/RunRecord.java
@@ -1,8 +1,12 @@
 package sg.np.edu.mad.ipptready;
 
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Date;
 
-public class RunRecord implements IPPTRecord {
+public class RunRecord extends AppCompatActivity implements IPPTRecord {
 
     // Total distance taken (can exceed 2.4km)
     public float TotalDistanceTravelled;
@@ -12,6 +16,18 @@ public class RunRecord implements IPPTRecord {
 
     // time taken to finsih the 2.4km
     public int TimeTakenFinished;
+
+    public void getRecord(){
+        Intent whiteHole = getIntent();
+        String dataToBeSent = whiteHole.getStringExtra("TimeTakenForTheRun");
+
+    }
+
+    private void writeToFirebase(){
+        //IPPTRecord -> collection
+        //RunRecord -> document
+
+    }
 
     @Override
     public int getIPPTRecordScore() {
