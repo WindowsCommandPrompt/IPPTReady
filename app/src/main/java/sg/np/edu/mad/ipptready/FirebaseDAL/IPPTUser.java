53347bd24f3243465d5961b570ca3cb1d2eb24bc
kuanzher <s10223870@connect.np.edu.sg>
2022-07-30 22:36:42 +0800

    Completed update function and profile picture upload and storage

--- app/src/main/java/sg/np/edu/mad/ipptready/FirebaseDAL/IPPTUser.java
+++ app/src/main/java/sg/np/edu/mad/ipptready/FirebaseDAL/IPPTUser.java
@@ -1,36 +1,39 @@
 package sg.np.edu.mad.ipptready.FirebaseDAL;
 
+import android.net.Uri;
+
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.SetOptions;
-import com.google.type.DateTime;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.Serializable;
-import java.sql.Time;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.jar.Attributes;
 
 public class IPPTUser implements Serializable {
     public static final String colFrom = "IPPTUser";
     private static final String NAME = "Name";
     private static final String DOB = "DOB";
+    private static final String IMAGE_KEY = "ImageKey";
     private static final String EMAIL_ADDRESS = "EmailAddress";
 
     public Date DoB;
     public String Name;
+    public Uri ProfilePicture;
+
+
+
 
     public IPPTUser(Map<String, Object> userMap) {
         Name = (String) userMap.get(NAME);
         DoB = ((Timestamp) userMap.get(DOB)).toDate();
     }
-
 
     public static DocumentReference getUserDocFromId(String userDocId) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -44,6 +47,8 @@ public class IPPTUser implements Serializable {
                 .document(EmailAddress)
                 .get();
     }
+
+    //public static StorageReference getUserProfilePicture(String EmailAddress){}
 
     public static FirebaseDocChange createNewUser(String EmailAddress,
                                                   String Name,
@@ -64,15 +69,29 @@ public class IPPTUser implements Serializable {
 
     public static Task<Void> updateUser(DocumentReference userDocRef,
                                         String Name,
-                                        Date Dob) {
+                                        Date Dob,
+                                        String imageKey,
+                                        byte[] data) {
 
         FirebaseDocChange updateUser = new FirebaseDocChange();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+
+
+        FirebaseStorage storage;
+        StorageReference storageReference;
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        StorageReference userRef = storageReference.child("profilePictures/" + imageKey);
+
+        UploadTask uploadTask = userRef.putBytes(data);
+
         Map<String, Object> updatedUserMap = new HashMap<>();
         updatedUserMap.put(NAME, Name);
         updatedUserMap.put(DOB, Dob);
-
+        updatedUserMap.put(IMAGE_KEY, imageKey);
         return updateUser.changeTask = userDocRef.set(updatedUserMap);
     }
 
--- app/src/main/java/sg/np/edu/mad/ipptready/ProfileActivity.java
+++ app/src/main/java/sg/np/edu/mad/ipptready/ProfileActivity.java
@@ -1,17 +1,16 @@
 package sg.np.edu.mad.ipptready;
 
-import static sg.np.edu.mad.ipptready.FirebaseDAL.IPPTUser.colFrom;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,25 +20,43 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-import java.text.DateFormat;
+import java.io.ByteArrayOutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
 
-import sg.np.edu.mad.ipptready.FirebaseDAL.FirebaseDocChange;
+import de.hdodenhof.circleimageview.CircleImageView;
 import sg.np.edu.mad.ipptready.FirebaseDAL.IPPTUser;
 
 public class ProfileActivity extends AppCompatActivity {
     private String EmailAddress;
     private IPPTUser user;
     final Calendar myCalendar= Calendar.getInstance();
+    CircleImageView profilePicture;
+    private static final int PICK_IMAGE = 100;
+    private static final int GET_IMAGE = 200;
+    Uri imageUri;
+    StorageReference pathReference;
+    private static FirebaseStorage storage;
+    private static StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,14 +81,15 @@ public class ProfileActivity extends AppCompatActivity {
         }
         else {
             // if all else fails...
+
             GenericErrorToast.show();
             finish();
         }
 
-
         // Get TextViews from ProfileActivity layout
         if (null != EmailAddress &&
             null != user) {
+            final String imageKey = UUID.randomUUID().toString();
 
             // Get TextViews to display the user information
             TextView name = findViewById(R.id.name);
@@ -79,8 +97,13 @@ public class ProfileActivity extends AppCompatActivity {
             TextView dob = findViewById(R.id.dateOfBirth);
             TextView mode = findViewById(R.id.userMode);
 
+            // Set subtext for user to change profile picture
+            TextView profilepictext = findViewById(R.id.profilePicText);
+            profilepictext.setText("Click Profile Picture To Change Picture");
+            profilepictext.setVisibility(View.GONE);
+
             // Get ImageViews to display profile picture and edit icon
-            ImageView profilePicture =findViewById(R.id.profilePicture);
+            profilePicture = findViewById(R.id.profilePicture);
             ImageView editProfile = findViewById(R.id.editImage);
 
             // Get Buttons for deleting and updating user information
@@ -96,6 +119,67 @@ public class ProfileActivity extends AppCompatActivity {
             // Get EditTexts to for user inputs
             EditText editName = findViewById(R.id.editTextName);
             EditText editDob = findViewById(R.id.editTextDob);
+
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            Task<DocumentSnapshot> task = db.collection("IPPTUser").document(EmailAddress).get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()){
+                                DocumentSnapshot userDoc = task.getResult();
+                                if (userDoc.exists()){
+                                    Map<String, Object> userMap = userDoc.getData();
+                                    String imageKey = null;
+                                    try{
+                                        imageKey = userMap.get("ImageKey").toString();
+                                        Log.v("IPPTUser", "IMAGE KEY::" + imageKey);
+                                    }
+                                    catch (NullPointerException n){
+                                        n.printStackTrace();
+                                    }
+                                    storage = FirebaseStorage.getInstance();
+                                    storageReference = storage.getReference();
+
+                                    pathReference = storageReference.child("profilePictures/" + imageKey);
+                                    Log.v("Profile Activity", "PATH REFERENCE: " + (pathReference == null));
+                                    if (pathReference != null)
+                                    {
+                                        final long maxBytes = 1024 * 1024;
+                                        pathReference.getBytes(maxBytes).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                                            @Override
+                                            public void onSuccess(byte[] bytes) {
+                                                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                                profilePicture.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 150, 150, false));
+                                            }
+                                        });
+                                    }
+                                    else
+                                    {
+                                        String cameraIcon = "@drawable/cameraicon";
+                                        int imageResource = getResources().getIdentifier(cameraIcon, null, getPackageName());
+                                        profilePicture.setImageDrawable(getResources().getDrawable(imageResource));
+                                    }
+
+
+                                }
+                                else {
+                                    Log.v("IPPTUser", "Doc does not exist::" + pathReference);
+                                }
+                            }
+                            else {
+                                Log.v("IPPTUser", "Task not successful::" + pathReference);
+                            }
+                        }
+                    });
+
+
+
+
+
+
+
 
             // Set EditTexts with user information as default
             editName.setText(user.Name);
@@ -117,6 +201,8 @@ public class ProfileActivity extends AppCompatActivity {
             editName.setVisibility(View.GONE);
             editDob.setVisibility(View.GONE);
 
+
+
             // Set OnClickListener for edit icon when user wants to update
             editProfile.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -137,6 +223,19 @@ public class ProfileActivity extends AppCompatActivity {
                     DatePickerDialog datePickerDialog= new DatePickerDialog(ProfileActivity.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH));
                     datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
 
+                    //Setting the Save and Cancel buttons and edittext fields to visible
+                    saveButton.setVisibility(View.VISIBLE);
+                    cancelButton.setVisibility(View.VISIBLE);
+                    editName.setVisibility(View.VISIBLE);
+                    editDob.setVisibility(View.VISIBLE);
+                    profilepictext.setVisibility(View.VISIBLE);
+
+                    //Setting the edit icon and delete button and textview fields to gone
+                    deleteButton.setVisibility(View.GONE);
+                    editProfile.setVisibility(View.GONE);
+                    name.setVisibility(View.GONE);
+                    dob.setVisibility(View.GONE);
+
                     editDob.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
@@ -144,17 +243,13 @@ public class ProfileActivity extends AppCompatActivity {
                         }
                     });
 
-                    //Setting the Save and Cancel buttons and edittext fields to visible
-                    saveButton.setVisibility(View.VISIBLE);
-                    cancelButton.setVisibility(View.VISIBLE);
-                    editName.setVisibility(View.VISIBLE);
-                    editDob.setVisibility(View.VISIBLE);
-
-                    //Setting the edit icon and delete button and textview fields to gone
-                    deleteButton.setVisibility(View.GONE);
-                    editProfile.setVisibility(View.GONE);
-                    name.setVisibility(View.GONE);
-                    dob.setVisibility(View.GONE);
+                    // set profile picture ImageView OnClickListener to open gallery to get image
+                    profilePicture.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            openGallery();
+                        }
+                    });
 
                     cancelButton.setOnClickListener(new View.OnClickListener() {
                         @Override
@@ -164,6 +259,7 @@ public class ProfileActivity extends AppCompatActivity {
                             cancelButton.setVisibility(View.GONE);
                             editName.setVisibility(View.GONE);
                             editDob.setVisibility(View.GONE);
+                            profilepictext.setVisibility(View.GONE);
 
                             //Setting the edit icon and delete button and textview fields to visible
                             deleteButton.setVisibility(View.VISIBLE);
@@ -171,6 +267,7 @@ public class ProfileActivity extends AppCompatActivity {
                             name.setVisibility(View.VISIBLE);
                             dob.setVisibility(View.VISIBLE);
                             mode.setText("Profile");
+                            profilePicture.setClickable(false);
                         }
                     });
                     saveButton.setOnClickListener(new View.OnClickListener() {
@@ -180,6 +277,7 @@ public class ProfileActivity extends AppCompatActivity {
                             cancelButton.setVisibility(View.GONE);
                             editName.setVisibility(View.GONE);
                             editDob.setVisibility(View.GONE);
+                            profilepictext.setVisibility(View.GONE);
 
                             //Setting the edit icon and delete button and textview fields to visible
                             deleteButton.setVisibility(View.VISIBLE);
@@ -187,6 +285,7 @@ public class ProfileActivity extends AppCompatActivity {
                             name.setVisibility(View.VISIBLE);
                             dob.setVisibility(View.VISIBLE);
                             mode.setText("Profile");
+                            profilePicture.setClickable(false);
 
                             Date dateOfBirth = null;
                             try {
@@ -195,9 +294,16 @@ public class ProfileActivity extends AppCompatActivity {
                                 e.printStackTrace();
                             }
 
+                            profilePicture.setDrawingCacheEnabled(true);
+                            profilePicture.buildDrawingCache();
+                            Bitmap bitmap = ((BitmapDrawable) profilePicture.getDrawable()).getBitmap();
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                            byte[] data = baos.toByteArray();
+
                             FirebaseFirestore db = FirebaseFirestore.getInstance();
                             DocumentReference userDocRef = db.collection("IPPTUser").document(EmailAddress);
-                            Task<Void> task = IPPTUser.updateUser(userDocRef,editName.getText().toString(),dateOfBirth);
+                            Task<Void> task = IPPTUser.updateUser(userDocRef,editName.getText().toString(),dateOfBirth, imageKey,data);
                             task.addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
@@ -216,7 +322,6 @@ public class ProfileActivity extends AppCompatActivity {
 
                         }
                     });
-
                 }
             });
 
@@ -262,9 +367,6 @@ public class ProfileActivity extends AppCompatActivity {
                 }
             });
 
-
-
-
             //========================================================================================================//
             //ADDED THE BELOW PART ON 15th June 2022 3:22AM
             //SEND EMAILADDRESS DATA OVER TO RunActivity.java
@@ -278,6 +380,19 @@ public class ProfileActivity extends AppCompatActivity {
             // missing data in intent or saveInstanceState
             GenericErrorToast.show();
             finish();
+        }
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            profilePicture.setImageURI(imageUri);
         }
     }
 
