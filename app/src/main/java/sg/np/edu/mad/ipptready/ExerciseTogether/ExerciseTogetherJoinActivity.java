4b2400b6fc5a068ce8753527b728b149479563e5
Bryan Koh <S10223617@connect.np.edu.sg>
2022-07-30 08:16:24 +0800

    Made QR Reader

--- app/src/main/java/sg/np/edu/mad/ipptready/ExerciseTogether/ExerciseTogetherJoinActivity.java
+++ app/src/main/java/sg/np/edu/mad/ipptready/ExerciseTogether/ExerciseTogetherJoinActivity.java
@@ -1,16 +1,146 @@
 package sg.np.edu.mad.ipptready.ExerciseTogether;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.util.Size;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.common.util.concurrent.ListenableFuture;
+import com.uk.tastytoasty.TastyToasty;
+
+import java.util.concurrent.ExecutionException;
+
+import sg.np.edu.mad.ipptready.LoginActivity;
 import sg.np.edu.mad.ipptready.R;
 
 public class ExerciseTogetherJoinActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
+    private Button qrCodeFoundButton;
+    private String qrCode;
+    private PreviewView cameraPreviewView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private CountDownTimer myCountDown;
+    private boolean countdownExecuted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_together_join);
+
+        qrCodeFoundButton = findViewById(R.id.qrCodeFoundButton);
+        qrCodeFoundButton.setVisibility(View.INVISIBLE);
+        qrCodeFoundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TastyToasty.makeText(ExerciseTogetherJoinActivity.this, qrCode, TastyToasty.SHORT,null, R.color.greendark, R.color.white, false).show();
+                Log.d(ExerciseTogetherJoinActivity.class.getSimpleName(), "QR Code Found: " + qrCode);
+            }
+        });
+
+        cameraPreviewView = findViewById(R.id.previewCameraJoinSession);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        requestCamera();
+    }
+
+    private void requestCamera() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startCamera();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                ActivityCompat.requestPermissions(ExerciseTogetherJoinActivity.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera();
+            } else {
+                TastyToasty.error(this, "Camera Permission Denied").show();
+            }
+        }
+    }
+
+    private void startCamera() {
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindCameraPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                TastyToasty.error(this, "Error starting camera " + e.getMessage()).show();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindCameraPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        cameraPreviewView.setPreferredImplementationMode(PreviewView.ImplementationMode.SURFACE_VIEW);
+
+        Preview preview = new Preview.Builder().build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(cameraPreviewView.createSurfaceProvider());
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(1280, 720))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new QRCodeImageAnalyzer(new QRCodeFoundListener() {
+            @Override
+            public void onQRCodeFound(String _qrCode) {
+                qrCode = _qrCode;
+                qrCodeFoundButton.setVisibility(View.VISIBLE);
+                if (!countdownExecuted)
+                {
+                    countDownTimer();
+                    countdownExecuted = true;
+                }
+            }
+
+            @Override
+            public void qrCodeNotFound() {
+                if (!countdownExecuted) qrCodeFoundButton.setVisibility(View.INVISIBLE);
+            }
+        }));
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageAnalysis, preview);
+    }
+
+    private void countDownTimer(){
+        myCountDown = new CountDownTimer(2500, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) { }
+
+            @Override
+            public void onFinish() { countdownExecuted = false; }
+        };
+        myCountDown.start();
     }
 }
